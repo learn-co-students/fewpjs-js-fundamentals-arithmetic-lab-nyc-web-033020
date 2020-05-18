cb3eeea873778c90d0b3a1971bc29cd03cb901cf
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-05-18 13:44:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
+
+const random = Math.random * 2
+
+const mod = 4 % 10
+
+const max = Math.max(...[1,2,3,4,5,20])
